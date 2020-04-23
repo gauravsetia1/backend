aecbd9e44c2159b243718d4a77af219892f7aa92
Gaurav Setia <gauravsetia1999@gmail.com>
2020-04-23 13:42:35 +0530

    commit

---
+++ src/main/java/com/example/backend/BackendApplication.java
@@ -0,0 +1,22 @@
+package com.example.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
+@RestController
+class testController {
+	@GetMapping("/")
+	String hello() {
+		return "hello world";
+	}
+}
